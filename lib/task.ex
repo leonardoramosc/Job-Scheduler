05cd979f17e30002728ef8d4088ce7e7a4b413a2
Leonardo Ramos <leorrc96@gmail.com>
2025-05-15 00:37:40 -0300

    execute tasks through post http request

--- lib/task.ex
+++ lib/task.ex
@@ -6,12 +6,12 @@ defmodule JobScheduler.Task do
   @allowed_statuses ~w(active inactive banned)
 
   schema "tasks" do
-    field :name, :string
-    field :url, :string
-    field :body, :map
-    field :schedule_time, :naive_datetime
-    field :status
-    field :created_at
+    field(:name, :string)
+    field(:url, :string)
+    field(:body, :map)
+    field(:schedule_time, :naive_datetime)
+    field(:status)
+    field(:created_at)
   end
 
   def changeset(task, params \\ %{}) do
@@ -28,6 +28,7 @@ defmodule JobScheduler.Task do
   defp validate_future_date(changeset, field) do
     validate_change(changeset, field, fn _, value ->
       today = NaiveDateTime.utc_now()
+
       case NaiveDateTime.compare(value, today) do
         :gt -> []
         _ -> [{field, "must be a future date"}]
@@ -46,8 +47,18 @@ defmodule JobScheduler.Task do
   end
 
   def execute(task) do
-    #HTTPoison.post(task.url, task.body)
     IO.inspect("executing task #{task.name}. url: #{task.url}")
-    :ok
+
+    with {:ok, body_str} <- Jason.encode(task.body),
+         {:ok, response} <- HTTPoison.post(task.url, body_str, [{"Content-Type", "application/json"}]) do
+      IO.inspect("task executed successfully. Response:")
+      IO.inspect(response)
+      :ok
+    else
+      {:error, error} ->
+        IO.inspect("unable to execute rask. Error:")
+        IO.inspect(error)
+        :error
+    end
   end
 end
